98f0096adb90fba617fb5878cfbdc9d1910b5a79
lucasjcarvalho <lucasjcarvalho@gmail.com>
2021-01-14 14:55:40 -0300

    testehtml-3

--- index.php
+++ index.php
@@ -1,8 +1,2 @@
-<p>Example of <b>Texto em negrito</b></p>
-<p>Example of <em>Texto enfatizado</em></p>
-<p>Example of <strong>Texto forte</strong></p>
-<p>Example of <i>Texto em italico</i></p>
-<p>Example of <sup>Texto superior</sup></p>
-<p>Example of <sub>Texto inferior</sub></p>
-<p>Example of <del>Texto taxado</del></p>
-<p>Example of <code>Texto de código de computador</code></p>
+<p><b>Teste de página com HTML</b></p>
+<p><em>Enfatizando um texto</em></p>
