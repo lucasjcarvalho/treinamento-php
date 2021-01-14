b7c33c21f1dfc2260fa5df21fa2bd2d7169ff8f1
lucasjcarvalho <lucasjcarvalho@gmail.com>
2021-01-14 15:06:14 -0300

    testehtml-6

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <p><b>Teste de página com HTML</b></p>
 <p><em>Enfatizando um texto</em></p>
 
-<a href="https://www.uol.com.br/">Visite a página do UOL</a></p>
+<a href="https://api.whatsapp.com/send?phone=5511999914321&text=Clique%20aqui%20para%20entrar%20em%20contato%20via%20Whatsapp">Clique Aqui</a></p>
 
 https://api.whatsapp.com/send?phone=5511999914321&text=Clique%20aqui%20para%20entrar%20em%20contato%20via%20Whatsapp
