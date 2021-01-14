db90d3f55e80508b2c1bb430914648c7b7e1e360
lucasjcarvalho <lucasjcarvalho@gmail.com>
2021-01-14 14:44:41 -0300

    testehtml-2

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<p>Hello and welcome to my motivation page. As Confucius once said:</p>
-
-<p>It does not matter how slowly you go as long as you do not stop.</p>
-
-<p>I also love the concept of positive thinking, and The Need To Eliminate Negative Self Talk
-(as mentioned in Affirmations for Positive Thinking.)</p>
+<p>Example of <b>Texto em negrito</b></p>
+<p>Example of <em>Texto enfatizado</em></p>
+<p>Example of <strong>Texto forte</strong></p>
+<p>Example of <i>Texto em italico</i></p>
+<p>Example of <sup>Texto superior</sup></p>
+<p>Example of <sub>Texto inferior</sub></p>
+<p>Example of <del>Texto taxado</del></p>
+<p>Example of <code>Texto de código de computador</code></p>
