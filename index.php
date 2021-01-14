be4b182f53694c08f16daeaebfa81feca99b29cd
lucasjcarvalho <lucasjcarvalho@gmail.com>
2021-01-14 15:07:35 -0300

    testehtml-6

--- index.php
+++ index.php
@@ -2,5 +2,3 @@
 <p><em>Enfatizando um texto</em></p>
 
 <a href="https://api.whatsapp.com/send?phone=5511999914321&text=Clique%20aqui%20para%20entrar%20em%20contato%20via%20Whatsapp">Clique Aqui</a></p>
-
-https://api.whatsapp.com/send?phone=5511999914321&text=Clique%20aqui%20para%20entrar%20em%20contato%20via%20Whatsapp
