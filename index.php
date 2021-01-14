63fa9d07d4672e23fb404e9d67ca431cbad1d4e2
lucasjcarvalho <lucasjcarvalho@gmail.com>
2021-01-14 14:38:42 -0300

    testehtml

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-Deploy simples com Continuous Delivery
+<p>Hello and welcome to my motivation page. As Confucius once said:</p>
+
+<p>It does not matter how slowly you go as long as you do not stop.</p>
+
+<p>I also love the concept of positive thinking, and The Need To Eliminate Negative Self Talk
+(as mentioned in Affirmations for Positive Thinking.)</p>
